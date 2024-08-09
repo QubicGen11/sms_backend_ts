f5a558b0137f401f03b502b496309714ae70b6b6
manojsravan <manojsravan2002@gmail.com>
2024-08-09 14:20:42 +0530

    updated code

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -61,11 +61,54 @@ class userOperations {
                 to: newUser.email as string, // Ensure email is string
                 subject: 'Welcome to Our Platform',
                 text: `Hello ${firstName},\n\nWelcome to our platform! Your account has been created successfully.\n\nBest Regards,\nTeam`,
-                html: `<p>Hello ${firstName},</p><p>Welcome to our platform! Your account has been created successfully.</p><p>Best Regards,<br>Team</p>`,
+                html: `<p>Hello ${firstName},</p><p>Welcome to our platform! Your account has been created successfully, please complete your details in this form <a></a></p><p>Best Regards,<br>Team</p>`,
             };
 
             await sendEmail(emailOptions);
 
+            res.status(201).json({ message: 'User created and invitation email sent', user: newUser });
+        } catch (error: any) {
+            logger.error('Error inviting user:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    };
+    static newUser = async (req: Request, res: Response) => {
+        const { firstName, lastName, email, role,phoneNumber, password, groupId, groupName } = req.body;
+
+        try {
+            // Check if the user already exists
+            const isUser = await prisma.user.findUnique({
+                where: {
+                    email: email,
+                },
+            });
+
+            if (isUser) {
+                return res.status(400).send('User already exists');
+            }
+
+            // Hash the password before storing it
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Create a new user in the database
+            const newUser = await prisma.user.create({
+                data: {
+                    name: `${firstName} ${lastName}`,
+                    email,
+                    phoneNumber,
+                    password: hashedPassword,
+                    role,
+                    // Assuming 'group' is a valid relation in your Prisma schema
+                    accessGroup: {
+                        connectOrCreate: {
+                            where: { id: groupId },
+                            create: { groupName: groupName },
+                        },
+                    },
+                },
+            });
+
+            // Send an email to the new user
             res.status(201).json({ message: 'User created and invitation email sent', user: newUser });
         } catch (error: any) {
             logger.error('Error inviting user:', error);
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,4 +4,5 @@ const router=Router()
 
 router.get('/allusers',userOperations.getAllUsers)
 router.post('/admininvite',userOperations.adminUserInvite)
+router.post('/newuser',userOperations.newUser)
 export default router
